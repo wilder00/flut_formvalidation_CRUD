94e09055dfe84b24afb2ae6256d85b467dab0801
Wilder Trujillo <nadie437@gmail.com>
2021-03-11 23:00:50 -0500

    mostrando una lista de los registros de productos de firebase en nuestra app

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -25,13 +25,34 @@ class HomePage extends StatelessWidget {
       builder:
           (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
         if (snapshot.hasData) {
-          return Container();
+          final productos = snapshot.data;
+          return ListView.builder(
+            itemCount: productos.length,
+            itemBuilder: (context, i) => _crearItem(context, productos[i]),
+          );
         } else {
           return Center(
             child: CircularProgressIndicator(),
           );
         }
       },
+    );
+  }
+
+  Widget _crearItem(BuildContext context, ProductoModel producto) {
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        color: Colors.red,
+      ),
+      onDismissed: (direccion) {
+        //TODO: borrar producto
+      },
+      child: ListTile(
+        title: Text('${producto.titulo} - ${producto.valor}'),
+        subtitle: Text(producto.id),
+        onTap: () => Navigator.pushNamed(context, 'producto'),
+      ),
     );
   }
 
